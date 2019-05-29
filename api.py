e39b78c76beb837e43f9515dac6958d3bf6fa87e
Osvaldo de Souza Ferreira <osvald.soza@gmail.com>
2019-05-29 12:13:35 -0300

    Update api.py

--- api.py
+++ api.py
@@ -1,16 +1,16 @@
 #  from requests_toolbelt import MultipartEncoder
 import requests
 
-MY_TOKEN = 'a6fd3a58aeb4aa726dbf7f25f63442db1c97f279'
+TOKEN = 'a6fd3a58aeb4aa726dbf7f25f63442db1c97f279'
 
 
 def get_json_api():
-    data = requests.get('https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=' + MY_TOKEN)
+    data = requests.get('https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=' + TOKEN)
     return data.encoding
 
 
 def post_json_api():
-    url = 'https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=' + MY_TOKEN
+    url = 'https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=' + TOKEN
     payload = 'name=\"answer\"; filename=\"answer.json\"\r\nContent-Type: application/json'
     headers = {
     'content-type': 'multipart/form-data;'
