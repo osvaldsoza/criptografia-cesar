4ecb0199d674dc57ce0657e81b50b438391883f6
osvaldsoza <osvald.soza@gmail.com>
2019-05-29 17:41:57 -0300

    comite

--- api.py
+++ api.py
@@ -10,10 +10,12 @@ def get_json_api():
 
 
 def post_json_api():
-    url = 'https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=' + MY_TOKEN
-    data = {'file': ('answer', open('answer.json', 'rb'))}
-    p = requests.post(url=url, data=data)
-    return p.text
+    url = 'https://api.codenation.dev/v1/challenge/dev-ps/submit-solution'
+    querystring = {'token': 'a6fd3a58aeb4aa726dbf7f25f63442db1c97f279'}
+    payload = {'Content-Disposition': 'form-data', 'file': 'answer', 'filename': 'answer.json', 'Content-Type': 'application/json'}
+    headers = {'Content-Type': 'multipart/form-data;'}
+    p = requests.request('POST', url=url, data=payload, headers=headers, params=querystring)
+    return p.status_code
 
 
 if __name__ == '__main__':
