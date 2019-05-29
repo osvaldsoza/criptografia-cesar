50cff4351b0b9fa55d533119e6adebf0a264fc4f
osvaldsoza <osvald.soza@gmail.com>
2019-05-29 17:51:12 -0300

    atualizando metodo post

--- api.py
+++ api.py
@@ -10,23 +10,12 @@ def get_json_api():
 
 
 def post_json_api():
-<<<<<<< HEAD
-    url = 'https://api.codenation.dev/v1/challenge/dev-ps/submit-solution'
-    querystring = {'token': 'a6fd3a58aeb4aa726dbf7f25f63442db1c97f279'}
-    payload = {'Content-Disposition': 'form-data', 'file': 'answer', 'filename': 'answer.json', 'Content-Type': 'application/json'}
-    headers = {'Content-Type': 'multipart/form-data;'}
-    p = requests.request('POST', url=url, data=payload, headers=headers, params=querystring)
-    return p.status_code
+    url = 'https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=a6fd3a58aeb4aa726dbf7f25f63442db1c97f279'
+    payload = {'file': 'answer', 'filename': 'answer.json'}
+    headers = {'Content-Type': 'multipart/form-data', 'Accept': 'application/json'}
+    p = requests.request('POST', url=url, data=payload, headers=headers)
+    return p.history
 
-=======
-    url = 'https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=' + TOKEN
-    payload = 'name=\"answer\"; filename=\"answer.json\"\r\nContent-Type: application/json'
-    headers = {
-    'content-type': 'multipart/form-data;'
-    }
-    response = requests.request('POST', url, data=payload, headers=headers)
-    return response.text
->>>>>>> e39b78c76beb837e43f9515dac6958d3bf6fa87e
 
 if __name__ == '__main__':
     print(post_json_api())
