aa7ca23d2354e28251260c5de589ab1fc6c9f215
osvaldsoza <osvald.soza@gmail.com>
2019-05-28 23:19:30 -0300

    atualizacao na api

--- api.py
+++ api.py
@@ -1,3 +1,4 @@
+#  from requests_toolbelt import MultipartEncoder
 import requests
 
 MY_TOKEN = 'a6fd3a58aeb4aa726dbf7f25f63442db1c97f279'
@@ -5,8 +6,15 @@ MY_TOKEN = 'a6fd3a58aeb4aa726dbf7f25f63442db1c97f279'
 
 def get_json_api():
     data = requests.get('https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=' + MY_TOKEN)
-    return data.json()
+    return data.encoding
+
+
+def post_json_api():
+    url = 'https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=' + MY_TOKEN
+    data = {'file': ('answer', open('answer.json', 'rb'))}
+    p = requests.post(url=url, data=data)
+    return p.text
 
 
 if __name__ == '__main__':
-    print(get_json_api())
+    print(post_json_api())
